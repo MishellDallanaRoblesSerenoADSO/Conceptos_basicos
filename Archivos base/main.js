c3e4215ca798670ce66ae22aa663b0da89c3a149
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2022-12-07 18:41:35 -0500

    Seccion1_ej6

--- Archivos base/main.js
+++ Archivos base/main.js
@@ -1,37 +1,26 @@
 addEventListener("DOMContentLoaded", (e)=>{
     
-    let tarea= document.getElementById("tarea")
-    let agregar= document.getElementById("agregar")
-    let listado= document.getElementById("listado")
-    let cantidad= document.getElementById("cantidad")
+    let seguir= document.getElementById("sig")
+    let volver= document.getElementById("ant")
 
-    total= 0
-    //funcion que agrega la tarea al listado
-    agregar.onclick = function(){
-        //Controla si el campo esta vacio
-        if(tarea.value == ""){
-            return;
+    sig.addEventListener('click',siguiente)
+    ant.addEventListener('click',anterior)
+
+    let instrucciones= [0," 1). Coloca el aceite en una olla grande a fuego bajo. Sofríe los tres pimentones picados, el ajo y la cebolla cabezona y revuelve hasta que tomen un color doradito.", "2). Agrega las 3 tazas de arroz, lo importante es revolver muy bien.", "3). Añade las 5 tazas de agua, las verduras en trocitos, las pechugas de pollo, el cubo de caldo de pollo, el sobre de sazón y la sal al gusto. Revuelve y deja cocinar a fuego medio por aproximadamente 15 minutos o hasta que se seque el arroz.", "4). Una vez que el arroz haya absorbido toda el agua tapa la olla y cocina a fuego bajo por 10 a 15 minutos más hasta que el arroz esté suave.", " Listo ya terminamos ahora disfruta de tu arroz!"]
+
+    let pinst= 0
+
+    function siguiente(){
+        pinst+=1
+        if(pinst <=5){
+            r.innerText= instrucciones[pinst]
         }
-        //valor del campo
-        let elemento= tarea.value
-        //crear un elemento li
-        let li= document.createElement("li")
-        li.textContent= elemento
-        //se agrega li al listado
-        listado.appendChild(li)
-        //cantidad de tareas
-        total++
-        cantidad.innerHTML=total
-
-        let eliminar= document.createElement("span")
-        eliminar.textContent= "\u00d7";
-        li.appendChild(eliminar);
-
-        eliminar.onclick= function(){
-            li.remove();
-            total--
-            cantidad.innerHTML=total
+    }
+    function anterior(){
+        pinst-=1
+        if(pinst >0){
+            r.innerText= instrucciones[pinst]
         }
-        tarea.value=""
+
     }
 })
